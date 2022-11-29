bfdc3e7697670b3f0b80df74ab14251bc0064194
Karol Wójcik <karol.wojcik@outlook.com>
2022-11-29 11:14:36 +0100

    traccar init

---
+++ traccar/rootfs/etc/cont-init.d/nginx.sh
@@ -0,0 +1,18 @@
+#!/command/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: Traccar
+# Configures NGINX for use with the Traccar server
+# ==============================================================================
+
+# Generate direct access configuration, if enabled.
+if bashio::var.has_value "$(bashio::addon.port 80)"; then
+    bashio::config.require.ssl
+    bashio::var.json \
+        certfile "$(bashio::config 'certfile')" \
+        keyfile "$(bashio::config 'keyfile')" \
+        port "^$(bashio::addon.port 80)" \
+        ssl "^$(bashio::config 'ssl')" \
+        | tempio \
+            -template /etc/nginx/templates/direct.gtpl \
+            -out /etc/nginx/servers/direct.conf
+fi
